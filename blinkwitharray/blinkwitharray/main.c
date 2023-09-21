5b82463b806ee981bf63a06756a4001bb55ee617
Arif Yardım <96440900+hotbagcom@users.noreply.github.com>
2023-09-21 12:53:24 +0300

    added pic and new func for seven_seg_disp

--- blinkwitharray/blinkwitharray/main.c
+++ blinkwitharray/blinkwitharray/main.c
@@ -13,11 +13,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void hard(uint8_t input);				
 
 int main()
-{
+{	//I made a mistake in here by using D0 and D1 pins 
+	//Which used by atmega 328p chip as UART connection
+	//so that I am using D 4,5,6,7 as A,B,C,D AND D 8,9,10,11 as E,F,G,(DP)
 	
-	uint8_t	 PD[10]={  0b0000110,//1
+	DDRD =0b01111111;	//0b(DP)GFEDCBA 
+						//DP for Dot Point but I did not use 
+	
+	uint8_t	 SSdp[10]={  
+					0b0111111,//0
+					0b0000110,//1
 					0b1011011,//2
 					0b1001111,//3
 					0b1100110,//4
@@ -26,23 +34,29 @@ int main()
 					0b0000111,//7
 					0b1111111,//8
 					0b1101111,//9
-					0b0111111//0
 					};
 					
-	DDRD =0b01111111;
-	uint8_t seg =0;
+	
+	uint8_t num =0;
 				while(1)
 				{
 					
-					for (seg = 0; seg<10 ;seg++)
+					for (num = 0; num<10 ;num++)
 					{
-						PORTD =  PD[seg];
-						_delay_ms(2000);
+						hard(SSdp[num]) ; 
+						_delay_ms(2000);			//wait for 2 second
 						
 					}
 						
-				}
-				
-			
-    
+				}    
 }
+
+void hard(uint8_t input){
+	DDRD = 0xF0; //I am not using D 0,1,2 and 3 pins , D 4,5,6,7 are out put from my chip
+	DDRB = 0x0F; //I'm given D 8,9,10 and 11 pins as output
+	PORTD = 0x00; //using this post for last four input bit
+	PORTB = 0x00; //using this post for first four input bit
+	
+	PORTB = (0x0F & input);		//take only first four bit 
+	PORTD = (0xF0 & input);		//take only last four bit
+}	
