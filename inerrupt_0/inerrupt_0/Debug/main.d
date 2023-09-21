7780b6fb14f112debb6a46295767f0a0e9f596f8
Arif Yardım <96440900+hotbagcom@users.noreply.github.com>
2023-09-21 15:35:25 +0300

    added picture and more comment

--- inerrupt_0/inerrupt_0/Debug/main.d
+++ inerrupt_0/inerrupt_0/Debug/main.d
@@ -12,9 +12,6 @@ main.d main.o: .././main.c \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\common.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\lock.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay_basic.h \
- c:\program\ files\microchip\xc8\v2.36\avr\avr\include\math.h \
  c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\interrupt.h
 
 c:\program\ files\microchip\xc8\v2.36\avr\lib\gcc\avr\5.4.0\include\stdint.h:
@@ -42,11 +39,5 @@ c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\common.h:
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\fuse.h:
 
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\lock.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\util\delay_basic.h:
-
-c:\program\ files\microchip\xc8\v2.36\avr\avr\include\math.h:
 
 c:\program\ files\microchip\xc8\v2.36\avr\avr\include\avr\interrupt.h:
--- inerrupt_0/inerrupt_0/main.c
+++ inerrupt_0/inerrupt_0/main.c
@@ -10,33 +10,44 @@
 
 #include <stdint.h>
 #include <avr/io.h>
-#include <util/delay.h>
+//#include <util/delay.h>  //we do not need delay 
 #include <avr/interrupt.h>
+
 uint8_t toogle ;
-ISR(INT0_vect){
-	PORTB = toogle<<5;
-	toogle = ~toogle; 
-	//EIFR &= ~(0x1<<INTF0);
+ISR(INT0_vect){//activate falling edge of input signal (when you release push button)
+	PORTB = toogle<<5;		//left shift toggle for 5 bit
+	toogle = ~toogle; //toogle==0  --->>> toogle= ~toogle  --->>> toogle= ~0 --->>> toogle= 1
+	
 }
-ISR(INT1_vect){
-	PORTB = toogle<<5;
+ISR(INT1_vect){//activate any logic chance of input signal
+	PORTB = toogle<<5;		//left shift toggle for 5 bit
 	toogle= ~toogle;
-	//EIFR &=  ~(0x1<<INTF1); 
+
 }
 
 int main(void)
 {
-	//pd2 in button
+	//pd2 input for button
 	DDRD = 0xFC; //0b1111 0011
-	//pb5 out led 
+	
+	//pb5 output for led 
 	DDRB  =0x20; //0b0010 0000
 	
-	EICRA = 0x06; //masks ISC11 and ISC10  ISC01 and ISC00 falling edge interupt 0b (INT1)01[any logical change] (INT0)10[_falling]
-	EIMSK = 0x03; // 
+	EICRA = 0x06;//0b0000 0110
+	//masks ISC11 and ISC10  ISC01 and ISC00 . 
+	//ISC1x is logic change interrupt
+	//ISC0x is falling edge interrupt 
+	//0b (INT1)01[any logical change] (INT0)10[_falling]
+	
+	EIMSK = 0x03; // enable int1 and int0
+	//Enable interrupt mask register
+	
 	sei();
 	
 	toogle =0x1;
-	while (1)
-	{
-	}
+	
+	//We do not need while because all precess done in ISR(interrupt service routine) 
+// 	while (1)
+// 	{
+// 	}
 }
